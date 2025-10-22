00521af93f05810c71aaecf919007cbb37eb0f9e
Santhosh-R2 <santhoshr.sics@gmail.com>
2025-10-22 10:27:50 +0530

    Add new routes for donor and hospital features

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -98,30 +98,65 @@ function App() {
           <Route path="/resetPassDoner/:id" element={<DonerResetPass />} />
           <Route path="/healthDetails" element={<AddHealthDetails />} />
           <Route path="/doner-medical-details" element={<MedicalInfo />} />
-          
+          <Route path="/donation-req" element={<DonerBloodRequest />} />
+          <Route path="/doner-dashboard" element={<DonerDashboard />} />
+          <Route path="/doner-Profile" element={<DonerProfile />} />
+          <Route path="/doner-edit-profile" element={<DonerEditProfile />} />
+          <Route path="/doner-completed-requests" element={<DonerDonationHistory />} />
+          <Route path="/doner-FullFilled" element={<Approving />} />
+          <Route path="/doner-ContactUs" element={<DonerContactUs />} />
+          <Route path="/doner-aboutus" element={<DonerAboutUs />} />
 
           {/* Admin route */}
           <Route path="/AdminLogin" element={<AdminLogin />} />
+          <Route path="/AdminDashBord" element={< AdminDashboard/>} />
+          <Route path="/doner-details/:id" element={<DonerDetails />} />
           <Route path="/Hospital-req" element={<HospitalReqt />} />
-          
+          <Route path="/approved-hospitals" element={<ApprovedHospitals />} />
+          <Route path="/view-doners" element={<ViewDoner />} />
+          <Route path="/emergency-alerts" element={<EmergencyAlert />} />
+          <Route path="/completed-requests" element={<CompletedRequests />} />
+          <Route path="/enquiries" element={<UserEnquiry />} />
+          <Route path="/notifications" element={<Notification />} />
+          <Route path="/blood-requests" element={<BloodRequests />} />
+          <Route path="/ViewAllUsers" element={<ViewAllUsers />} />
 
 
           {/* Hospital route */}
 
           <Route path="/hospitalRegistration" element={<HosRegistration />} />
           <Route path="/hospitalInfo" element={<HospitalInfo />} />
+          <Route path="/Hospital-Dashboard" element={<HosDashboard />} />
+
           <Route path="/hosLogin" element={<HosLogin />} />
           <Route path="/forgotPass" element={<Forgot />} />
           <Route path="/resetPass/:id" element={<HosResetPass />} />
-          
+          <Route path="/hosProfile" element={<HospitalProfile />} />
+          <Route path="/hosEditProfile" element={<EditHospital />} />
+          <Route path="/bloodrequesthos" element={<BloodRequestHos />} />
+          <Route path="/editBloodReq/:id" element={<EditBloodReq />} />
+          <Route path="/willingDoners" element={<WilligDoners />} />
+          <Route path="/hosCompletedReq" element={<HosCompletedReq />} />
+          <Route path="/hosEmergency" element={<AllBloodRequest />} />
+          <Route path="/manageUserBlood" element={<ManageUserBlood />} />
+          <Route path="/approvedRequests" element={<ApprovedRequest />} />
+          <Route path="/canceledRequests" element={<CalceledRequest />} />
 
 
 
           <Route path="/UserRegistration" element={<UserRegistration />} />
           <Route path="/UserLogin" element={<UserLogin />} />
+          <Route path="/UserDashboard" element={<UserDashboard />} />
           <Route path="/forgotPassuser" element={<UserForgotPass />} />
           <Route path="/resetPassuser/:id" element={<UserResetPassword />} />
-          
+          <Route path="/user-profile" element={<UserProfile />} />
+          <Route path="/user-edit-profile" element={<UserEditProfile />} />
+          <Route path="/user-blood-request" element={<UserBloodReq />} />
+          <Route path="/user-requests" element={<RequestHistory />} />
+          <Route path="/user-edit-request/:id" element={<EditUserRequest />} />
+          <Route path="/user-HospitalList" element={<HospitalList />} />
+          <Route path="/user-view-requests" element={<ViewRequests />} />
+          <Route path="/user-view-notifications" element={<UserNotification />} />
 
           <Route path="/ChatBot" element={<ChatBot />} />
 
